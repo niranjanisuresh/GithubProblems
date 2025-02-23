eebd1f8a161726ea241d82be398a3aed9c3dbb68
Niranjani S <Niranjani S@LAPTOP-P6A43PVN>
2025-02-23 17:42:01 +0530

    FirstCommit

--- src/TCS_Questions/Ques_1.java
+++ src/TCS_Questions/Ques_1.java
@@ -1,10 +1,18 @@
 package TCS_Questions;
-
+import java.util.Scanner;
 public class Ques_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan=new Scanner(System.in);
+		
+		int T=scan.nextInt();
+		
+		for(int i=0;i<T;i++) {
+			
+			int n=scan.nextInt();
+			int handshakes = (n*n-1)/2;
+			System.out.println(handshakes);
+			
+		}
 	}
-
 }
